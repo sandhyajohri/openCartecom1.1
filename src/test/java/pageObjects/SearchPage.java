8f02e2f0e4a1f65528ef63d3391a8280f1e18191
Sandhya <sandhyajohri@gmail.com>
2025-06-18 11:57:33 -0500

    third commit

---
+++ src/test/java/pageObjects/SearchPage.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class SearchPage extends BasePage{
+
+	public SearchPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
